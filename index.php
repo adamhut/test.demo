922077727b09ef30e5ac560f208a938f0844c25a
adamhut <ahuang@bacera.com>
2013-10-07 07:29:44 -0700

    inital_commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include('bigtocn.class.php');
+
+
+$obj=new bigtocn();
+$obj->set_folder('tranlate_folder');
+//$obj->set_file('test2.php');
+//$obj->set_file('mobiletrader.php');
+$obj->travel_folder();
+//$obj->convert_big_to_cn();
+
+?>
